f03c7c1ac817c15b2952b55c094f9a250ec2623b
Nicoquiro <165960676+Nicoquiro@users.noreply.github.com>
2025-04-13 11:51:28 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,5 +1,16 @@
 def slice_simple():
     texto = "Awesome"
+
+    texto = texto.lower()
+
+    primera = texto[0:3]
+    las_cuatro_iniciales = texto[0:4]
+    segunda = texto[2:5]
+    ultima = texto[4:7]
+
+    print(primera)
+    print(segunda)
+    print(las_cuatro_iniciales + ultima)
     # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
 

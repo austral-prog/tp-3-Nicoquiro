d0503d54e0492f69ce7caaa6aba11399642c7917
Nicoquiro <165960676+Nicoquiro@users.noreply.github.com>
2025-04-13 11:59:50 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -5,8 +5,4 @@ def check_vowels():
     
     for vowel in vowels:
         print(f"Contiene {vowel}: {vowel in name_lower}")
-    # Código a implementar utilizando input.
-
-
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
+    

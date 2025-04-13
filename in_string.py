fb1b8e317030049e2b0dd3d0b36fd715807ed842
Nicoquiro <165960676+Nicoquiro@users.noreply.github.com>
2025-04-13 11:49:30 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,4 +1,10 @@
 def check_vowels():
+    name = input("Ingresa un nombre: ")
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    name_lower = name.lower()
+    
+    for vowel in vowels:
+        print(f"Contiene {vowel}: {vowel in name_lower}")
     # Código a implementar utilizando input.
 
 

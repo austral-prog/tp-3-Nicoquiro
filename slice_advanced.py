974b1e8ee2287c440e16eaf0408ec06caf8c9eee
Nicoquiro <165960676+Nicoquiro@users.noreply.github.com>
2025-04-13 12:00:07 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -2,7 +2,4 @@ def slice_advanced():
     texto = input("Ingrese un texto: ")
     resultado = texto[4::2]
     print(resultado)
-    # Código a implementar utilizando input.
-
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
+    
